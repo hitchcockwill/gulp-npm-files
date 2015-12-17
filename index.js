37fbc0ef0fc7b705345750e2bac7e35545318937
Will Hitchcock <hitchcock.will@gmail.com>
2015-12-17 08:11:11 -0700

    added option to include only dist files

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 var fs = require('fs');
 
-module.exports = function(devDependencies, packageJsonFilePath) {
+module.exports = function(devDependencies, packageJsonFilePath, opts) {
   var buffer, packages, keys;
-  
+
+  opts = opts || {};
+
   buffer = fs.readFileSync(packageJsonFilePath || './package.json');
   packages = JSON.parse(buffer.toString());
   keys = [];
-  
+
   for (key in packages.dependencies) {
-    keys.push('./node_modules/' + key + '/**/*');
+    if (opts.includeDist) {
+      var package = JSON.parse(fs.readFileSync('./node_modules/' + key + '/package.json'));
+      keys.push('./node_modules/' + key + '/' + package.main);
+    } else {
+      keys.push('./node_modules/' + key + '/**/*');
+    }
   }
 
   if (devDependencies) {
